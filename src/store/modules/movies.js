d59e8ba2b855ce35a3fa80641e8bd32f1cde8633
camopu <aleks.revati@gmail.com>
2020-10-04 18:33:39 +0300

    response interceptors

--- src/store/modules/movies.js
+++ src/store/modules/movies.js
@@ -1,18 +1,28 @@
+import IDs from "@/store/mock/imdb_top250";
 import axios from "@/plugins/axios";
 
 const moviesStore = {
   namespaced: true,
-  state: {},
-  getters: {},
+  state: {
+    top250IDs: IDs,
+    moviesPerPage: 12,
+    currentPage: 1
+  },
+  getters: {
+    slicedIDs: ({ top250IDs }) => (from, to) => top250IDs.slice(from, to),
+    currentPage: ({ currentPage }) => currentPage,
+    moviesPerPage: ({ moviesPerPage }) => moviesPerPage
+  },
   mutations: {},
   actions: {
-    async fetchMovies(context) {
-      console.log(context);
-      const response = await axios.get("/", {
-        params: {
-          i: "tt0111161"
-        }
-      });
+    async fetchMovies({ getters }) {
+      const { currentPage, moviesPerPage, slicedIDs } = getters;
+      const from = currentPage * moviesPerPage - moviesPerPage;
+      const to = currentPage * moviesPerPage;
+      const moviesToFatch = slicedIDs(from, to);
+
+      const requests = moviesToFatch.map(id => axios.get(`/?i=${id}`));
+      const response = await Promise.all(requests);
       console.log(response);
     }
   }
