2da19465955834d73a82d7aeabf2cc048146dcdd
camopu <aleks.revati@gmail.com>
2020-10-05 20:04:29 +0300

    movies list getter

---
+++ src/components/MoviesList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "MoviesList",
+  props: {
+    list: {
+      type: Object,
+      default: () => ({})
+    }
+  }
+};
+</script>
+
+<style scoped></style>
