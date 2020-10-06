57f0b093607ff2eb9c0cc21e06f804494e229091
camopu <aleks.revati@gmail.com>
2020-10-06 19:46:07 +0300

    added pagination

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,12 @@
   <div id="app">
     <PosterBg :poster="posterBg" />
     <MoviesList :list="moviesList" @changePoster="onChangePoster" />
+    <MoviesPagination
+      :current-page="currentPage"
+      :per-page="moviesPerPage"
+      :total="moviesLength"
+      @pageChanged="onPageChanged"
+    />
   </div>
 </template>
 
@@ -9,24 +15,34 @@
 import { mapActions, mapGetters } from "vuex";
 import MoviesList from "@/components/MoviesList";
 import PosterBg from "@/components/PosterBg";
+import MoviesPagination from "@/components/MoviesPagination";
 
 export default {
   name: "App",
   components: {
     MoviesList,
-    PosterBg
+    PosterBg,
+    MoviesPagination
   },
   data: () => ({
     posterBg: ""
   }),
   computed: {
-    ...mapGetters("movies", ["moviesList"])
+    ...mapGetters("movies", [
+      "moviesList",
+      "currentPage",
+      "moviesPerPage",
+      "moviesLength"
+    ])
   },
   methods: {
-    ...mapActions("movies", ["fetchMovies"]),
+    ...mapActions("movies", ["changeCurrentPage"]),
     onChangePoster(poster) {
       console.log(poster);
       this.posterBg = poster;
+    },
+    onPageChanged(page) {
+      this.changeCurrentPage(page);
     }
   }
 };
