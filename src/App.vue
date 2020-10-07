08d4765bd51d36922ecf0365bf3b7b490524170c
camopu <aleks.revati@gmail.com>
2020-10-07 17:34:54 +0300

    preloader

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <Loader />
     <PosterBg :poster="posterBg" />
     <MoviesList :list="moviesList" @changePoster="onChangePoster" />
     <MoviesPagination
@@ -16,13 +17,15 @@ import { mapActions, mapGetters } from "vuex";
 import MoviesList from "@/components/MoviesList";
 import PosterBg from "@/components/PosterBg";
 import MoviesPagination from "@/components/MoviesPagination";
+import Loader from "@/components/Loader";
 
 export default {
   name: "App",
   components: {
     MoviesList,
     PosterBg,
-    MoviesPagination
+    MoviesPagination,
+    Loader
   },
   data: () => ({
     posterBg: ""
